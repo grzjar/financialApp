a5912185c58d5abdaa89d6f663af4796bdb2ef32
grzegorzjaroszewski <jaroszewski.g@gmail.com>
2021-06-18 19:13:48 +0200

    1 - commit

---
+++ src/test/java/pl/grzjar/financialApp/FinancialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.grzjar.financialApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
